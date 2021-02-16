8b5374a963ccb526f29b19217e02ba99d4156490
Daniel <danielsaraldi@gmail.com>
2021-02-16 15:21:41 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,33 +1,34 @@
 import { SignUpController } from './signup';
 import { MissingParamError } from '../errors/missing-param-error';
 
-describe('SignUp Controller', () => { // Controller name being tested
-    test('Should return 400 if no name is provided', () => {
-        const sut = new SignUpController()
-        const httpRequest = {
-            body: {
-                email: 'any_email@mail.com',
-                password: 'any_password',
-                passwordConfirmation: 'any_password'
-            }
-        }
-        const httpResponse = sut.handle(httpRequest)
-        // Expect return status code 400 
-        expect(httpResponse.statusCode).toBe(400) // toBe: Checks whether the value data in the object is identical
-        expect(httpResponse.body).toEqual(new MissingParamError('name')) // toEqual: Checks if the values ​​are equal 
-    })
+describe('SignUp Controller', () => {
+  // Controller name being tested
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    // Expect return status code 400
+    expect(httpResponse.statusCode).toBe(400); // toBe: Checks whether the value data in the object is identical
+    expect(httpResponse.body).toEqual(new MissingParamError('name')); // toEqual: Checks if the values ​​are equal
+  });
 
-    test('Should return 400 if no email is provided', () => {
-        const sut = new SignUpController()
-        const httpRequest = {
-            body: {
-                name: 'any_name',
-                password: 'any_password',
-                passwordConfirmation: 'any_password'
-            }
-        }
-        const httpResponse = sut.handle(httpRequest)
-        expect(httpResponse.statusCode).toBe(400) 
-        expect(httpResponse.body).toEqual(new MissingParamError('email')) 
-    })
-})
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('email'));
+  });
+});
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,19 +1,12 @@
 import { HttpRequest, HttpResponse } from '../protocols/http';
 import { MissingParamError } from '../errors/missing-param-error';
+import { badRequest } from '../helpers/http-helper';
 
 export class SignUpController {
-    handle (httpRequest: HttpRequest): HttpResponse {
-        if (!httpRequest.body.name) {
-            return {
-                statusCode: 400,
-                body: new MissingParamError('name')
-            }
-        }
-        if (!httpRequest.body.email) {
-            return {
-                statusCode: 400,
-                body: new MissingParamError('email')
-            }
-        }
-    }
+  handle(httpRequest: HttpRequest): HttpResponse {
+    if (!httpRequest.body.name)
+      return badRequest(new MissingParamError('name'));
+    if (!httpRequest.body.email)
+      return badRequest(new MissingParamError('email'));
+  }
 }
