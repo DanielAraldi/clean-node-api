8caa8f36f8a190bfb34f344a6f9a53a49a98c163
Daniel <danielsaraldi@gmail.com>
2021-02-19 20:01:43 -0300

    fix: concerts path

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from './signup';
 import { MissingParamError, InvalidParamError, ServerError } from '../../errors';
-import { EmailValidator, AccountModel, AddAccount, AddAccountModel } from './../signup-protocols';
+import { EmailValidator, AccountModel, AddAccount, AddAccountModel } from './signup-protocols';
 
 const makeEmailValidatorStub = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
