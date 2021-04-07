811cb3c20fe5cf4fcd0d588e9cde0529228542b5
Daniel <danielsaraldi@gmail.com>
2021-04-06 21:05:09 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -107,4 +107,20 @@ describe("DbAddAccount Usecase", () => {
     const promise = sut.add(accountData);
     await expect(promise).rejects.toThrow();
   });
+
+  test("Should return an account on success", async () => {
+    const { sut } = makeSut();
+    const accountData = {
+      name: "valid_name",
+      email: "valid_mail",
+      password: "valid_password",
+    };
+    const account = await sut.add(accountData);
+    expect(account).toEqual({
+      id: "valid_id",
+      name: "valid_name",
+      email: "valid_mail",
+      password: "hashed_password",
+    });
+  });
 });
