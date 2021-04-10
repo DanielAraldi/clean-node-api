1e7951337119c2ba465cf26ad3a4328fc7273aca
Daniel <danielsaraldi@gmail.com>
2021-04-09 21:22:39 -0300

    refactor: add protocols to account

---
+++ src/infra/db/mongodb/account-repository/account-protocols.ts
@@ -0,0 +1,4 @@
+export * from "../../../../data/protocols/add-account-repository";
+export { AddAccountModel } from "../../../../domain/usecases/add-account";
+export * from "../../../../domain/models/account";
+export * from "../helpers/mongodb-helper";
--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,7 +1,9 @@
-import { AddAccountRepository } from "../../../../data/protocols/add-account-repository";
-import { AddAccountModel } from "../../../../domain/usecases/add-account";
-import { AccountModel } from "../../../../domain/models/account";
-import { MongoHelper } from "../helpers/mongodb-helper";
+import {
+  AccountModel,
+  AddAccountModel,
+  AddAccountRepository,
+  MongoHelper,
+} from "./account-protocols";
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
