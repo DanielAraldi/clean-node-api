e2375e9c260a2ce8e896942dd8e5f8a227af454e
Daniel <danielsaraldi@gmail.com>
2021-04-09 21:02:28 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -3,11 +3,18 @@ import { AccountMongoRepository } from "./account";
 
 describe("Account Mongo Repository", () => {
   beforeAll(async () => {
+    // Before all tests connect to mongodb
     await MongoHelper.connect(process.env.MONGO_URL);
   });
 
   afterAll(async () => {
+    // After all tests disconnect to mongodb
     await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    await accountCollection.deleteMany({}); // Delete all register of the table
   });
 
   const makeSut = (): AccountMongoRepository => new AccountMongoRepository();
