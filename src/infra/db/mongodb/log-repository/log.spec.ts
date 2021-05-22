47cee856babdb35c8f9cac003fefb2d8ee1afb0c
Daniel <danielsaraldi@gmail.com>
2021-05-21 22:03:47 -0300

    test: should create an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,30 @@
+import { Collection } from "mongodb";
+import { MongoHelper } from "../helpers/mongodb-helper";
+import { LogMongoRepository } from "./log";
+
+describe("Log Mongo Repository", () => {
+  let errorCollection: Collection;
+
+  beforeAll(async () => {
+    // Before all tests connect to mongodb
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    // After all tests disconnect to mongodb
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection("errors");
+    await errorCollection.deleteMany({}); // Delete all register of the table
+  });
+
+  test("Should create an error log on success", async () => {
+    const sut = new LogMongoRepository();
+    await sut.logError("any_error");
+    // Returns the amount of data in a database
+    const count = await errorCollection.countDocuments();
+    expect(count).toBe(1);
+  });
+});
