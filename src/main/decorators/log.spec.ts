2e74aff31e4d52412129d4bd5c3b4f4875b67a96
Daniel <danielsaraldi@gmail.com>
2021-05-21 21:36:15 -0300

    refactor: put best name to log error

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -19,7 +19,7 @@ const makeController = (): Controller => {
 
 const makeLogErrorRepository = (): LogErrorRepository => {
   class LogErrorRepositoryStub implements LogErrorRepository {
-    async log(stack: string): Promise<void> {
+    async logError(stack: string): Promise<void> {
       return new Promise((resolve) => resolve());
     }
   }
@@ -84,7 +84,7 @@ describe("LogController Decorator", () => {
 
   test("Should call LogErrorRepository with correct error if controller returns a server error", async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
-    const logSpy = jest.spyOn(logErrorRepositoryStub, "log");
+    const logSpy = jest.spyOn(logErrorRepositoryStub, "logError");
     jest
       .spyOn(controllerStub, "handle")
       .mockReturnValueOnce(
