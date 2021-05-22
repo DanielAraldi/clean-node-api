ffceb89abb493ce512756fda8053739b3fff10d6
Daniel <danielsaraldi@gmail.com>
2021-05-21 22:03:26 -0300

    feat: should create an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -0,0 +1,12 @@
+import { LogErrorRepository } from "../../../../data/protocols/log-error-repository";
+import { MongoHelper } from "../helpers/mongodb-helper";
+
+export class LogMongoRepository implements LogErrorRepository {
+  async logError(stack: string): Promise<void> {
+    const errorCollection = await MongoHelper.getCollection("errors");
+    errorCollection.insertOne({
+      stack,
+      data: new Date(),
+    });
+  }
+}
