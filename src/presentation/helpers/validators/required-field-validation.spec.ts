657ee6dc85361ca87c33a5b0c5fb97d203af9340
Daniel <danielsaraldi@gmail.com>
2021-06-16 19:41:12 -0300

    test: add tests to CompareFieldsValidation

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,5 +1,5 @@
-import { MissingParamError } from "../../errors";
 import { RequiredFieldValidation } from "./required-field-validation";
+import { MissingParamError } from "../../errors";
 
 const makeSut = (): RequiredFieldValidation => {
   return new RequiredFieldValidation("field");
@@ -8,13 +8,13 @@ const makeSut = (): RequiredFieldValidation => {
 describe("Required Field Validation", () => {
   test("Should return a MissingParamError if validation fails", () => {
     const sut = makeSut();
-    const error = sut.validate({ invalidField: "any_data" });
+    const error = sut.validate({ invalidField: "any_value" });
     expect(error).toEqual(new MissingParamError("field"));
   });
 
   test("Should not return if validation succeds", () => {
     const sut = makeSut();
-    const error = sut.validate({ field: "any_data" });
+    const error = sut.validate({ field: "any_value" });
     expect(error).toBeFalsy();
   });
 });
