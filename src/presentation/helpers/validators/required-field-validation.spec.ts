2f5ef6c3e8e7e4782697c050cd34c7854ab610a8
Daniel <danielsaraldi@gmail.com>
2021-06-16 19:32:56 -0300

    test: ensure RequiredFieldValidation not return if validation succeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe("Required Field Validation", () => {
     const error = sut.validate({ invalidField: "any_data" });
     expect(error).toEqual(new MissingParamError("field"));
   });
+
+  test("Should not return if validation succeds", () => {
+    const sut = new RequiredFieldValidation("field");
+    const error = sut.validate({ field: "any_data" });
+    expect(error).toBeFalsy();
+  });
 });
