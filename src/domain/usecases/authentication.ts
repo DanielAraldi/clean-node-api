309c508724931403017ecb472b98659e03582119
Daniel <danielsaraldi@gmail.com>
2021-06-17 20:36:07 -0300

    refactor: change Authentication to receive a model

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,3 +1,8 @@
+export interface AuthenticationModel {
+  email: string;
+  password: string;
+}
+
 export interface Authentication {
-  auth(email: string, password: string): Promise<string>;
+  auth(autentications: AuthenticationModel): Promise<string>;
 }
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -28,7 +28,7 @@ export class LoginController implements Controller {
         return badRequest(error);
       }
       const { email, password } = httpRequest.body;
-      const accessToken = await this.authentication.auth(email, password);
+      const accessToken = await this.authentication.auth({ email, password });
       if (!accessToken) {
         return unauthorized();
       }
