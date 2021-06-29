13d5b457415f853ef2cf2b5df8aa4493f04b2f8d
Daniel <danielsaraldi@gmail.com>
2021-06-29 20:41:46 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/clean-node-api",
   port: process.env.PORT || 3333,
+  jwtSecret: process.env.JWT_SECRET || "tj67O==5H",
 };
