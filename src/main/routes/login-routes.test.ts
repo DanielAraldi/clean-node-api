144637794333a4e02506c9a429f9b07f243d9884
Daniel <danielsaraldi@gmail.com>
2021-06-29 20:59:25 -0300

    test: ensure login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -51,5 +51,14 @@ describe("Login Routes", () => {
         })
         .expect(200);
     });
+
+    test("Should return 401 on login", async () =>
+      await request(app)
+        .post("/api/login")
+        .send({
+          email: "daniel@gmail.com",
+          password: "123",
+        })
+        .expect(401));
   });
 });
