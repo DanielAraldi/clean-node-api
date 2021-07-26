187d652668446fa3e03ae1d55e4c50ae0cf18661
Daniel <danielsaraldi@gmail.com>
2021-07-26 20:59:05 -0300

    feat: ensure SignUpController returns 403 if AddAccount returns null

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -6,7 +6,13 @@ import {
   AddAccount,
   Validation,
 } from "./signup-controller-protocols";
-import { badRequest, serverError, ok } from "../../helpers/http/http-helper";
+import {
+  badRequest,
+  serverError,
+  ok,
+  forbidden,
+} from "../../helpers/http/http-helper";
+import { EmailInUseError } from "../../errors";
 
 export class SignUpController implements Controller {
   constructor(
@@ -23,11 +29,14 @@ export class SignUpController implements Controller {
       }
       const { name, email, password } = httpRequest.body;
 
-      await this.addAccount.add({
+      const account = await this.addAccount.add({
         name,
         email,
         password,
       });
+      if (!account) {
+        return forbidden(new EmailInUseError());
+      }
       const accessToken = await this.authentication.auth({
         email,
         password,
--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -1,3 +1,4 @@
+export * from "./email-in-use-error";
 export * from "./invalid-param-error";
 export * from "./missing-param-error";
 export * from "./server-error";
