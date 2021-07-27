7687b6040362f5c101bd1ee0006536310dad02d6
Daniel <danielsaraldi@gmail.com>
2021-07-27 20:00:13 -0300

    refactor: separate factories in folders

--- src/main/factories/controllers/login/login-validation-factory.ts
+++ src/main/factories/controllers/login/login-validation-factory.ts
@@ -2,9 +2,9 @@ import {
   ValidationComposite,
   EmailValidation,
   RequiredFieldValidation,
-} from "../../../presentation/helpers/validators";
-import { Validation } from "../../../presentation/protocols/validation";
-import { EmailValidatorAdapter } from "../../adapters/validators/email-validator-adapter";
+} from "../../../../presentation/helpers/validators";
+import { Validation } from "../../../../presentation/protocols/validation";
+import { EmailValidatorAdapter } from "../../../adapters/validators/email-validator-adapter";
 
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = [];
--- src/main/factories/controllers/signup/signup-validation-factory.ts
+++ src/main/factories/controllers/signup/signup-validation-factory.ts
@@ -3,9 +3,9 @@ import {
   CompareFieldsValidation,
   EmailValidation,
   RequiredFieldValidation,
-} from "../../../presentation/helpers/validators";
-import { Validation } from "../../../presentation/protocols/validation";
-import { EmailValidatorAdapter } from "../../adapters/validators/email-validator-adapter";
+} from "../../../../presentation/helpers/validators";
+import { Validation } from "../../../../presentation/protocols/validation";
+import { EmailValidatorAdapter } from "../../../adapters/validators/email-validator-adapter";
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = [];
