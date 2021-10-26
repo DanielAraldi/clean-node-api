10f35d978b702bc2de5d2ff41ddddf133fabe47a
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2021-10-26 19:52:27 -0300

    feat: ensure AddSurveyController calls AddSurvey usecase with correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
@@ -1,2 +1,3 @@
 export * from "../../../protocols";
 export * from "../../../helpers/http/http-helper";
+export * from "../../../../domain/usecases/add-survey";
--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,4 +1,5 @@
 import {
+  AddSurvey,
   badRequest,
   Controller,
   HttpRequest,
@@ -7,13 +8,21 @@ import {
 } from "./add-survey-controller-protocols";
 
 export class AddSurveyController implements Controller {
-  constructor(private readonly validation: Validation) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest);
+    const error = this.validation.validate(httpRequest.body);
     if (error) {
       return badRequest(error);
     }
-    return new Promise((resolve) => resolve(null));
+    const { question, answers } = httpRequest.body;
+    await this.addSurvey.add({
+      question,
+      answers,
+    });
+    return null;
   }
 }
