db06542499e16ee8f88fcb49ca77c195bfa921f2
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2021-11-08 19:49:25 -0300

    test: ensure AuthMiddleware returns 403 if LoadAccountByToken returns null

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -51,4 +51,13 @@ describe("Auth Middleware", () => {
     await sut.handle(makeFakeRequest());
     expect(loadSpy).toHaveBeenCalledWith("any_token");
   });
+
+  test("Should return 403 if LoadAccountByToken returns null", async () => {
+    const { sut, loadAccountByTokenStub } = makeSut();
+    jest
+      .spyOn(loadAccountByTokenStub, "load")
+      .mockRejectedValueOnce(new Promise((resolve) => resolve(null)));
+    const httpResponse = await sut.handle({});
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
+  });
 });
