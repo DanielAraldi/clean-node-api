9ccc1e0282949eba8915d4e2d897fc6e726cdfca
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2021-11-09 20:34:12 -0300

    feat: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository with correct values

--- src/data/protocols/db/account/load-account-by-email-repository.ts
+++ src/data/protocols/db/account/load-account-by-email-repository.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from "../../../usecases/add-account/db-add-account-protocols";
+import { AccountModel } from "../../../../domain/models/account";
 
 export interface LoadAccountByEmailRepository {
   loadByEmail(email: string): Promise<AccountModel>;
