8db21ad4de6e779752bd06c1846e4a6db8495d1a
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2021-11-15 09:58:29 -0300

    test: ensure JwtAdapter call verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -5,6 +5,10 @@ jest.mock("jsonwebtoken", () => ({
   async sign(): Promise<string> {
     return new Promise((resolve) => resolve("any_token"));
   },
+
+  async verify(): Promise<string> {
+    return new Promise((resolve) => resolve("any_value"));
+  },
 }));
 
 const makeSut = (): JwtAdapter => new JwtAdapter("secret");
@@ -31,6 +35,15 @@ describe("Jwt Adapter", () => {
       });
       const promise = sut.encrypt("any_id");
       await expect(promise).rejects.toThrow();
+    });
+  });
+
+  describe("verify()", () => {
+    test("Should call verify with correct values", async () => {
+      const sut = makeSut();
+      const verifySpy = jest.spyOn(jwt, "verify");
+      await sut.decrypt("any_token");
+      expect(verifySpy).toHaveBeenCalledWith("any_token", "secret");
     });
   });
 });
