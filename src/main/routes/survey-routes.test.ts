11d4899fdfd3d7ff10c5a908ec1b24353a14c5a8
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2021-11-18 22:01:38 -0300

    feat: ensure add survey route returns 200 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,12 @@
 import request from "supertest";
 import app from "../config/app";
+import env from "../config/env";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongodb-helper";
 import { Collection } from "mongodb";
+import { sign } from "jsonwebtoken";
 
 let surveyCollection: Collection;
+let accountCollection: Collection;
 
 describe("Survey Routes", () => {
   beforeAll(async () => {
@@ -17,10 +20,12 @@ describe("Survey Routes", () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection("surveys");
     await surveyCollection.deleteMany({});
+    accountCollection = await MongoHelper.getCollection("accounts");
+    await accountCollection.deleteMany({});
   });
 
   describe("POST /surveys", () => {
-    test("Should return 204 on add survey without accessToken", async () =>
+    test("Should return 403 on add survey without accessToken", async () =>
       await request(app)
         .post("/api/surveys")
         .send({
@@ -36,5 +41,33 @@ describe("Survey Routes", () => {
           ],
         })
         .expect(403));
+
+    test("Should return 204 on add survey with valid accessToken", async () => {
+      const account = await accountCollection.insertOne({
+        name: "Daniel",
+        email: "daniel@gmail.com",
+        password: "123",
+        role: "admin",
+      });
+      const id = account.insertedId;
+      const accessToken = sign({ id }, env.jwtSecret);
+      await accountCollection.updateOne({ _id: id }, { $set: { accessToken } });
+      await request(app)
+        .post("/api/surveys")
+        .set("x-access-token", accessToken)
+        .send({
+          question: "Question",
+          answers: [
+            {
+              answer: "Answer 1",
+              image: "http://image-name.com",
+            },
+            {
+              answer: "Answer 2",
+            },
+          ],
+        })
+        .expect(204);
+    });
   });
 });
