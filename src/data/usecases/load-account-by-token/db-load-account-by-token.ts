acf48035b27265c08d74e8147444bf5116fdc37f
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2021-11-18 21:58:19 -0300

    fix: removing the decrypted token from the loadAccountByTokenRepository and adding the user access token

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -15,7 +15,7 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
     const token = await this.decrypter.decrypt(accessToken);
     if (token) {
       const account = await this.loadAccountByTokenRepository.loadByToken(
-        token,
+        accessToken,
         role
       );
       if (account) {
