c232e3915f18f0bc86e1d5e82dc74eca635199f7
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2021-11-20 13:48:14 -0300

    fix: ensure AccountMongoRepository allows admin to acess any route

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -43,7 +43,7 @@ export class AccountMongoRepository
     const accountCollection = await MongoHelper.getCollection("accounts");
     const account = await accountCollection.findOne({
       accessToken: token,
-      role,
+      $or: [{ role }, { role: "admin" }],
     });
     return account && MongoHelper.map(account);
   }
