58cb96d61e563714610194e7ac5b6e82a577a797
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-04-21 10:51:52 -0300

    feat: change AddSurvey to insert a date also

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -25,6 +25,7 @@ export class AddSurveyController implements Controller {
       await this.addSurvey.add({
         question,
         answers,
+        date: new Date(),
       });
       return noContent();
     } catch (error) {
