a639c4982ad417de49b6ea7fa24fef4e5fe0dae4
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-04-21 12:02:04 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -27,20 +27,34 @@ const makeFakeSurveys = (): SurveyModel[] => [
   },
 ];
 
+const makeLoadSurveys = (): LoadSurveys => {
+  class LoadSurveysStub implements LoadSurveys {
+    async load(): Promise<SurveyModel[]> {
+      return new Promise((resolve) => resolve(makeFakeSurveys()));
+    }
+  }
+  return new LoadSurveysStub();
+};
+
+interface SutTypes {
+  sut: LoadSurveysController;
+  loadSurveysStub: LoadSurveys;
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveysStub = makeLoadSurveys();
+  const sut = new LoadSurveysController(loadSurveysStub);
+  return { sut, loadSurveysStub };
+};
+
 describe("LoadSurveys Controller", () => {
   beforeAll(() => MockDate.set(new Date()));
 
   afterAll(() => MockDate.reset());
 
   test("Should call LoadSurveys", async () => {
-    class LoadSurveysStub implements LoadSurveys {
-      async load(): Promise<SurveyModel[]> {
-        return new Promise((resolve) => resolve(makeFakeSurveys()));
-      }
-    }
-    const loadSurveysStub = new LoadSurveysStub();
+    const { sut, loadSurveysStub } = makeSut();
     const loadSpy = jest.spyOn(loadSurveysStub, "load");
-    const sut = new LoadSurveysController(loadSurveysStub);
     await sut.handle({});
     expect(loadSpy).toHaveBeenCalled();
   });
