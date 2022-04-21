eaa5cc744637f5c0c377d2fff6b28e8e9d789f0a
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-04-21 11:52:36 -0300

    feat: ensure LoadSurveysController call LoadSurveysUseCase

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller-protocols.ts
@@ -0,0 +1,3 @@
+export { Controller, HttpRequest, HttpResponse } from "../../../protocols";
+export * from "../../../../domain/usecases/load-surveys";
+export { SurveyModel } from "../../../../domain/models/survey";
---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,15 @@
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  LoadSurveys,
+} from "./load-surveys-controller-protocols";
+
+export class LoadSurveysController implements Controller {
+  constructor(private readonly loadSurveys: LoadSurveys) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load();
+    return null;
+  }
+}
