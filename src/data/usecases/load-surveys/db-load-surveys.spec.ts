fdac8b1f3e63a288a48f47bbf1d0ab69188aea12
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-05-21 10:26:40 -0300

    test: ensure DbLoadSurveys returns a list of surveys on success

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -57,4 +57,10 @@ describe("DbLoadSurveys", () => {
     await sut.load();
     expect(loadAllSpy).toHaveBeenCalled();
   });
+
+  test("Should return a list of survey on success", async () => {
+    const { sut } = makeSut();
+    const surveys = await sut.load();
+    expect(surveys).toEqual(makeFakeSurveys());
+  });
 });
