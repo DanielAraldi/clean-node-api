b58716729f80f1e0d9bf59b2d5dc1e3914638ef7
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-07-21 22:38:18 -0300

    fix: fixed and added auth Middleware exceptional data storage

--- src/main/factories/middlewares/auth-middleware-factory.ts
+++ src/main/factories/middlewares/auth-middleware-factory.ts
@@ -1,6 +1,9 @@
 import { AuthMiddleware } from "../../../presentation/middlewares/auth-middleware";
 import { Middleware } from "../../../presentation/protocols";
+import { makeLogControllerDecorator } from "../decorators/log-controller-decorator-factory";
 import { makeDbLoadAccountByToken } from "../usecases/account/load-account-by-token/db-load-account-by-token-factory";
 
-export const makeAuthMiddleware = (role?: string): Middleware =>
-  new AuthMiddleware(makeDbLoadAccountByToken(), role);
+export const makeAuthMiddleware = (role?: string): Middleware => {
+  const authMiddleware = new AuthMiddleware(makeDbLoadAccountByToken(), role);
+  return makeLogControllerDecorator(authMiddleware);
+};
