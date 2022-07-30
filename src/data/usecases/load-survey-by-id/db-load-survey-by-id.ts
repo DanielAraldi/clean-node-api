3869316c8488a89379dccedf57baa8ad250496f0
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-07-30 17:12:05 -0300

    feat: ensure DbLoadSurveyById calls LoadSurveyByIdRepository with correct id

---
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.ts
@@ -0,0 +1,14 @@
+import { LoadSurveyById } from "@/domain/usecases/load-survey-by-id";
+import { SurveyModel } from "@/domain/models/survey";
+import { LoadSurveyByIdRepository } from "@/data/protocols/db/survey/load-survey-by-id-repository";
+
+export class DbLoadSurveyById implements LoadSurveyById {
+  constructor(
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
+  ) {}
+
+  async loadById(id: string): Promise<SurveyModel | null> {
+    await this.loadSurveyByIdRepository.loadById(id);
+    return null;
+  }
+}
