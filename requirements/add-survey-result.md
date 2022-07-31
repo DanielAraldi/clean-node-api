ce721965eecfe8ef064cbcb8f1cc8b9a27386533
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-07-31 10:43:49 -0300

    feat: ensure SaveSurveyResultController returns 500 if LoadSurveyById throws

--- requirements/add-survey-result.md
+++ requirements/add-survey-result.md
@@ -18,4 +18,4 @@
 1. ⛔️ Retorna erro **403** se a resposta enviada pelo client for uma resposta inválida
 1. ⛔️ Retorna erro **500** se der erro ao tentar criar o resultado da enquete
 1. ⛔️ Retorna erro **500** se der erro ao tentar atualizar o resultado da enquete
-1. ⛔️ Retorna erro **500** se der erro ao tentar carregar a enquete
+1. ✅ Retorna erro **500** se der erro ao tentar carregar a enquete
--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -5,19 +5,24 @@ import {
   LoadSurveyById,
   InvalidParamError,
   forbidden,
+  serverError,
 } from "./save-survey-result-controller-protocols";
 
 export class SaveSurveyResultController implements Controller {
   constructor(private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const survey = await this.loadSurveyById.loadById(
-      httpRequest.params?.surveyId
-    );
+    try {
+      const survey = await this.loadSurveyById.loadById(
+        httpRequest.params?.surveyId
+      );
 
-    if (!survey) {
-      return forbidden(new InvalidParamError("surveyId"));
+      if (!survey) {
+        return forbidden(new InvalidParamError("surveyId"));
+      }
+      return null;
+    } catch (error) {
+      return serverError(error);
     }
-    return null;
   }
 }
