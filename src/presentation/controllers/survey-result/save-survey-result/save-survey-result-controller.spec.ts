40f806140902ec1170311fa60084dbd271f3089a
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-07-31 11:49:35 -0300

    test: ensure SaveSurveyResultController returns 200 if SaveSurveyResult on success

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -9,6 +9,7 @@ import {
   SaveSurveyResult,
   SaveSurveyResultModel,
   SurveyResultModel,
+  ok,
 } from "./save-survey-result-controller-protocols";
 import MockDate from "mockdate";
 
@@ -140,5 +141,11 @@ describe("SaveSurveyResult Controller", () => {
       );
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  test("Should return 200 on success", async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(ok(makeFakeSurveyResult()));
   });
 });
