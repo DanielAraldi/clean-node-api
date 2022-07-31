fabd4bd6f33c9593b42ad0d300f3eacc947a8d9a
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-07-31 10:36:01 -0300

    test: ensure SaveSurveyResultController retruns 403 if invalid surveyId is provided

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
@@ -1,3 +1,5 @@
 export * from "@/domain/usecases/survey/load-survey-by-id";
 export * from "@/domain/models/survey";
+export * from "@/presentation/errors";
+export * from "@/presentation/helpers/http/http-helper";
 export * from "@/presentation/protocols";
--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -3,13 +3,21 @@ import {
   HttpRequest,
   HttpResponse,
   LoadSurveyById,
+  InvalidParamError,
+  forbidden,
 } from "./save-survey-result-controller-protocols";
 
 export class SaveSurveyResultController implements Controller {
   constructor(private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveyById.loadById(httpRequest.params?.surveyId);
+    const survey = await this.loadSurveyById.loadById(
+      httpRequest.params?.surveyId
+    );
+
+    if (!survey) {
+      return forbidden(new InvalidParamError("surveyId"));
+    }
     return null;
   }
 }
