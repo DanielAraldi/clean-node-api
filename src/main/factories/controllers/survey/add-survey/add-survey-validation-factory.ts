cc2a77672874d6089e03013aba94649c12232f6a
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-08-11 21:36:52 -0300

    feat: added multiple values validation to validate array object properties of a survey

--- src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.ts
@@ -2,10 +2,16 @@ import { Validation } from "@/presentation/protocols/validation";
 import {
   ValidationComposite,
   RequiredFieldValidation,
+  MultipleValuesValidation,
 } from "@/validation/validators";
 
 export const makeAddSurveyValidation = (): ValidationComposite => {
   const validations: Validation[] = [];
   validations.push(new RequiredFieldValidation(["question", "answers"]));
+  validations.push(
+    new MultipleValuesValidation("answers", [
+      new RequiredFieldValidation(["answer"]),
+    ])
+  );
   return new ValidationComposite(validations);
 };
