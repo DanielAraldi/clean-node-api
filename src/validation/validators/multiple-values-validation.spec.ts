de5bb766760a37435dde7c3dc76824e7138edb50
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-08-11 21:09:47 -0300

    refactor: move ValidationStub to factory methods

--- src/validation/validators/multiple-values-validation.spec.ts
+++ src/validation/validators/multiple-values-validation.spec.ts
@@ -2,39 +2,41 @@ import { MissingParamError } from "@/presentation/errors/missing-param-error";
 import { Validation } from "@/presentation/protocols";
 import { MultipleValuesValidation } from "./multiple-values-validation";
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(input: any): Error | null {
+      return null;
+    }
+  }
+  return new ValidationStub();
+};
+
+type SutTypes = {
+  sut: MultipleValuesValidation;
+  validationStub: Validation;
+};
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation();
+  const sut = new MultipleValuesValidation("objectFieldName", [validationStub]);
+  return { sut, validationStub };
+};
+
 describe("Multiple Values Validation", () => {
   test("Should return a MissingParamError if objectFieldName validation fails", () => {
-    class ValidationStub implements Validation {
-      constructor(private readonly fieldNames: string[]) {}
-      validate(input: any): Error | null {
-        return null;
-      }
-    }
-    const validationStub = new ValidationStub(["field"]);
+    const { sut, validationStub } = makeSut();
     jest
       .spyOn(validationStub, "validate")
       .mockReturnValueOnce(new MissingParamError("objectFieldName"));
-    const sut = new MultipleValuesValidation("objectFieldName", [
-      validationStub,
-    ]);
     const error = sut.validate({ invalidField: [{ field: "any_value" }] });
     expect(error).toEqual(new MissingParamError("objectFieldName"));
   });
 
   test("Should return a MissingParamError if fieldNames validation fails", () => {
-    class ValidationStub implements Validation {
-      constructor(private readonly fieldNames: string[]) {}
-      validate(input: any): Error | null {
-        return null;
-      }
-    }
-    const validationStub = new ValidationStub(["field"]);
+    const { sut, validationStub } = makeSut();
     jest
       .spyOn(validationStub, "validate")
       .mockReturnValueOnce(new MissingParamError("field"));
-    const sut = new MultipleValuesValidation("objectFieldName", [
-      validationStub,
-    ]);
     const error = sut.validate({
       objectFieldName: [{ invalidField: "any_value" }],
     });
