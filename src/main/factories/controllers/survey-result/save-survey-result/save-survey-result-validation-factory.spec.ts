bd867b22fd1b62bbc637f37338755622c3110c43
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-08-13 13:04:31 -0300

    test: ensure makeSaveSurveyResultValidation calls ValidationComposite with all validations

---
+++ src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-validation-factory.spec.ts
@@ -0,0 +1,17 @@
+import { Validation } from "@/presentation/protocols/validation";
+import {
+  RequiredFieldValidation,
+  ValidationComposite,
+} from "@/validation/validators";
+import { makeSaveSurveyResultValidation } from "./save-survey-result-validation-factory";
+
+jest.mock("@/validation/validators/validation-composite");
+
+describe("SaveSurveyResultValidation Factory", () => {
+  test("Should call ValidationComposite with all validations", () => {
+    makeSaveSurveyResultValidation();
+    const validations: Validation[] = [];
+    validations.push(new RequiredFieldValidation(["answer"]));
+    expect(ValidationComposite).toHaveBeenCalledWith(validations);
+  });
+});
