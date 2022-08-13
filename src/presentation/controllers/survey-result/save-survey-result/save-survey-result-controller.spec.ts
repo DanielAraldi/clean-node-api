a53b1bfbe06d1931e371d5448bdc076ce16415b7
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-08-13 12:55:16 -0300

    test: ensure SaveSurveyResultController returns 400 if Validation fails

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -11,6 +11,7 @@ import {
   SurveyResultModel,
   ok,
   Validation,
+  badRequest,
 } from "./save-survey-result-controller-protocols";
 import MockDate from "mockdate";
 
@@ -101,6 +102,13 @@ describe("SaveSurveyResult Controller", () => {
     const httpRequest = makeFakeRequest();
     await sut.handle(httpRequest);
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  test("Should return 400 if Validation fails", async () => {
+    const { sut, validationStub } = makeSut();
+    jest.spyOn(validationStub, "validate").mockReturnValueOnce(new Error());
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(badRequest(new Error()));
   });
 
   test("Should call LoadSurveyById with correct values", async () => {
