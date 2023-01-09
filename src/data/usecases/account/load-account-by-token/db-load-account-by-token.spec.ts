5681eda4d1436abab75a4791677e6fc178aa525c
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-01-09 20:00:23 -0300

    test: change DbLoadAccountByToken to returns null if Decrypter throws

--- src/data/usecases/account/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/account/load-account-by-token/db-load-account-by-token.spec.ts
@@ -65,11 +65,11 @@ describe('DbLoadAccountByToken Usecase', () => {
     expect(account).toEqual(loadAccountByTokenRepositorySpy.accountModel);
   });
 
-  test('Should throw if Decrypter throws', async () => {
+  test('Should return null if Decrypter throws', async () => {
     const { sut, decrypterSpy } = makeSut();
     jest.spyOn(decrypterSpy, 'decrypt').mockImplementationOnce(throwError);
-    const promise = sut.load(token, role);
-    await expect(promise).rejects.toThrow();
+    const account = await sut.load(token, role);
+    expect(account).toBeNull();
   });
 
   test('Should throw if LoadAccountByTokenRepository throws', async () => {
