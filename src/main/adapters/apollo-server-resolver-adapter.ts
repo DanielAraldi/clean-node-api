cf2b9903f620ffe95bc532e7e9d36b59411bc987
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-01-25 18:17:35 -0300

    feat: add status code error on exceptions

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -1,9 +1,38 @@
 import { Controller } from '@/presentation/protocols';
+import { GraphQLError } from 'graphql';
 
 export const adaptResolver = async (
   controller: Controller,
   args: any
 ): Promise<any> => {
   const httpResponse = await controller.handle(args);
-  return httpResponse.body;
+  switch (httpResponse.statusCode) {
+    case 200:
+    case 204:
+      return httpResponse.body;
+    case 400:
+      throw new GraphQLError(httpResponse.body.message, {
+        extensions: {
+          code: 'BAD_REQUEST',
+        },
+      });
+    case 401:
+      throw new GraphQLError(httpResponse.body.message, {
+        extensions: {
+          code: 'UNAUTHENTICATED',
+        },
+      });
+    case 403:
+      throw new GraphQLError(httpResponse.body.message, {
+        extensions: {
+          code: 'FORBIDDEN',
+        },
+      });
+    default:
+      throw new GraphQLError(httpResponse.body.message, {
+        extensions: {
+          code: 'INTERNAL_SERVER_ERROR',
+        },
+      });
+  }
 };
--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -1,16 +1,44 @@
 import { Express } from 'express';
-import { apolloExpress, headers } from '@/main/middlewares';
 import { ApolloServer } from '@apollo/server';
+import { GraphQLError } from 'graphql';
+import { apolloExpress, headers } from '@/main/middlewares';
 import { pluginDrainHttpServer } from '../graphql/plugins';
 import typeDefs from '../graphql/type-defs';
 import resolvers from '../graphql/resolvers';
+
+const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
+  errors?.forEach(error => {
+    if (checkError(error, 'BAD_REQUEST')) {
+      response.http.status = 400;
+    } else if (checkError(error, 'UNAUTHENTICATED')) {
+      response.http.status = 401;
+    } else if (checkError(error, 'FORBIDDEN')) {
+      response.http.status = 403;
+    } else {
+      response.http.status = 500;
+    }
+  });
+};
+
+const checkError = (error: GraphQLError, errorName: string): boolean => {
+  return error.extensions.code === errorName;
+};
 
 export default (app: Express): void => {
   headers(app);
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    plugins: [pluginDrainHttpServer(app)],
+    plugins: [
+      pluginDrainHttpServer(app),
+      {
+        requestDidStart: async () => ({
+          willSendResponse: async ({ response, errors }) => {
+            handleErrors(response, errors);
+          },
+        }),
+      },
+    ],
   });
   server
     .start()
