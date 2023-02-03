b764ce9b1e422ade2392373d5315065bca247cb4
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-02-03 08:49:48 -0300

    test(#16): ensure RefreshTokenController returns 400 if Validation returns an error

--- tests/presentation/controllers/refresh-token-controller.spec.ts
+++ tests/presentation/controllers/refresh-token-controller.spec.ts
@@ -1,6 +1,9 @@
 import { RefreshTokenController } from '@/presentation/controllers';
 import { mockRefreshTokenParams } from '@/tests/domain/mocks';
 import { ValidationSpy } from '@/tests/presentation/mocks';
+import { MissingParamError } from '@/presentation/errors';
+import { badRequest } from '@/presentation/helpers';
+import { faker } from '@faker-js/faker';
 
 type SutTypes = {
   validationSpy: ValidationSpy;
@@ -22,5 +25,12 @@ describe('Refresh Token Controller', () => {
     const request = mockRequest();
     await sut.handle(request);
     expect(validationSpy.input).toEqual(request);
+  });
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationSpy } = makeSut();
+    validationSpy.error = new MissingParamError(faker.random.word());
+    const httpResponse = await sut.handle(mockRequest());
+    expect(httpResponse).toEqual(badRequest(validationSpy.error));
   });
 });
