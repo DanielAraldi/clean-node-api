8e0e3bae19891da5be9400a53a75c251ea0422ab
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-02-03 08:46:08 -0300

    feat(#16): ensure RefreshTokenController returns 400 if Validation returns an error

--- src/presentation/controllers/refresh-token-controller.ts
+++ src/presentation/controllers/refresh-token-controller.ts
@@ -1,10 +1,14 @@
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols';
+import { badRequest } from '@/presentation/helpers';
 
 export class RefreshTokenController implements Controller {
   constructor(private readonly validation: Validation) {}
 
   async handle(request: RefreshTokenController.Request): Promise<HttpResponse> {
-    this.validation.validate(request);
+    const error = this.validation.validate(request);
+    if (error) {
+      return badRequest(error);
+    }
     return Promise.resolve(null);
   }
 }
