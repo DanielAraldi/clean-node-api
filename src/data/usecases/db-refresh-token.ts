b1082bf0e3461746e5f0b02a9e3a4917c0993164
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-02-05 18:04:11 -0300

    feat(#16): ensure DbRefreshToken UseCase calls Encrypter with correct plaintext

--- src/data/usecases/db-refresh-token.ts
+++ src/data/usecases/db-refresh-token.ts
@@ -1,13 +1,19 @@
 import { RefreshToken } from '@/domain/usecases';
-import { LoadAccountByTokenRepository } from '@/data/protocols';
+import { Encrypter, LoadAccountByTokenRepository } from '@/data/protocols';
 
 export class DbRefreshToken implements RefreshToken {
   constructor(
-    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
+    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository,
+    private readonly encrypter: Encrypter
   ) {}
 
   async refresh(accessToken: string): Promise<RefreshToken.Result> {
-    await this.loadAccountByTokenRepository.loadByToken(accessToken);
+    const account = await this.loadAccountByTokenRepository.loadByToken(
+      accessToken
+    );
+    if (account) {
+      this.encrypter.encrypt(account.id);
+    }
     return Promise.resolve(null);
   }
 }
