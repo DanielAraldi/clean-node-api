11cb128e8ec5766c0884c848589bf838bf3afb7f
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-02-28 17:00:44 -0300

    test(#25): ensure AlmostRequiredFieldValidation returns null if a field is provided

--- tests/validation/validators/almost-required-fields.spec.ts
+++ tests/validation/validators/almost-required-fields.spec.ts
@@ -9,11 +9,17 @@ const makeSut = (): AlmostRequiredFieldValidation =>
   new AlmostRequiredFieldValidation([firstField, secondField]);
 
 describe('Required Field Validation', () => {
-  test('Should return a MissingParamError if no field is passed', () => {
+  test('Should return a MissingParamError if no field is provided', () => {
     const sut = makeSut();
     const error = sut.validate({ invalidField: faker.random.word() });
     expect(error).toEqual(
       new MissingParamError([firstField, secondField].join(' or '))
     );
+  });
+
+  test('Should returns null if a field is provided', () => {
+    const sut = makeSut();
+    const error = sut.validate({ [firstField]: faker.random.word() });
+    expect(error).toBeNull();
   });
 });
